aac174175085ea12d2c86587d2255bd16609b2ca
Felipe Sena <felipedepaulasena@hotmail.com>
2024-05-03 20:17:59 -0300

    Update java config

--- lua/plugins/lang/java.lua
+++ lua/plugins/lang/java.lua
@@ -1,23 +1,26 @@
 return {
-  {
+  "nvim-java/nvim-java",
+  config = function()
+    require("java").setup()
+    require("lspconfig").jdtls.setup({})
+  end,
+  dependencies = {
+    "nvim-java/lua-async-await",
+    "nvim-java/nvim-java-core",
+    "nvim-java/nvim-java-test",
+    "nvim-java/nvim-java-dap",
+    "MunifTanjim/nui.nvim",
     "neovim/nvim-lspconfig",
-    opts = {
-      -- make sure mason installs the server
-      servers = {
-        jdtls = {},
-      },
-      setup = {
-        jdtls = function()
-          return true -- avoid duplicate servers
-        end,
+    "mfussenegger/nvim-dap",
+    "nvim-java/nvim-java-refactor",
+    {
+      "williamboman/mason.nvim",
+      opts = {
+        registries = {
+          "github:nvim-java/mason-registry",
+          "github:mason-org/mason-registry",
+        },
       },
     },
   },
-  {
-    "williamboman/mason.nvim",
-    opts = {
-      ensure_installed = { "java-test", "java-debug-adapter" },
-    },
-  },
-  { "folke/which-key.nvim" },
 }
