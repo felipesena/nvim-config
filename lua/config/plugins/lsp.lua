5ffa604125312e0becf7b724942ef342e27a3a41
Felipe Sena <felipedepaulasena@hotmail.com>
2024-12-12 19:42:52 -0300

    chore: improve lsp configuration

--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -11,6 +11,13 @@ return {
           },
         },
       },
+      { 'williamboman/mason.nvim', config = true },
+      'williamboman/mason-lspconfig.nvim',
+      'WhoIsSethDaniel/mason-tool-installer.nvim',
+      -- Useful status updates for LSP.
+      { 'j-hui/fidget.nvim',       opts = {} },
+      -- Allows extra capabilities provided by nvim-cmp
+      'hrsh7th/cmp-nvim-lsp',
     },
     config = function()
       require("lspconfig").lua_ls.setup {}
@@ -30,8 +37,54 @@ return {
               end,
             })
           end
+
+          ---@diagnostic disable-next-line: missing-parameter, param-type-mismatch
+          if client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
+            local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
+            vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+              buffer = args.buf,
+              group = highlight_augroup,
+              callback = vim.lsp.buf.document_highlight,
+            })
+
+            vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+              buffer = args.buf,
+              group = highlight_augroup,
+              callback = vim.lsp.buf.clear_references,
+            })
+
+            vim.api.nvim_create_autocmd('LspDetach', {
+              group = vim.api.nvim_create_augroup('kickstart-lsp-detach', { clear = true }),
+              callback = function(event2)
+                vim.lsp.buf.clear_references()
+                vim.api.nvim_clear_autocmds { group = 'kickstart-lsp-highlight', buffer = event2.buf }
+              end,
+            })
+          end
+
+          ---@diagnostic disable-next-line: param-type-mismatch
+          if client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
+            vim.keymap.set("n", "<leader>th",
+              function() vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hist.is_enabled { bufnr = args.buf }) end)
+          end
         end,
       })
+
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
+
+      local servers = {
+        pyright = {},
+        lua_ls = {
+          settings = {
+            Lua = {
+              completion = {
+                callSnippet = 'Replace',
+              }
+            }
+          }
+        }
+      }
     end,
   }
 }
