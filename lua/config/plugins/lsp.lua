055d1b24bbb2497b9d2dccf3a28f147b1f2fbc16
personal <felipedepaulasena@hotmail.com>
2024-12-12 16:40:18 -0300

    chore: add keymap

--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -7,7 +7,7 @@ return {
         ft = "lua", -- only load on lua files
         opts = {
           library = {
-            { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+            { path = "luvit-meta/library", words = { "vim%.uv" } },
           },
         },
       },
--- plugin/keymaps.lua
+++ plugin/keymaps.lua
@@ -5,5 +5,6 @@ keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
 keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
 keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
 
-vim.keymap.set("n", "<space>x", ":.lua<CR>")
-vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+keymap.set("n", "<space>x", ":.lua<CR>")
+keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
+keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
