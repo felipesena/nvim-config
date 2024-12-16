ca402112aa55a6adda3490331c322b61032826ba
Felipe Sena <felipedepaulasena@hotmail.com>
2024-12-16 09:44:23 -0300

    chore: add terminal keymaps

--- plugin/keymaps.lua
+++ plugin/keymaps.lua
@@ -8,3 +8,15 @@ keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
 keymap.set("n", "<space>x", ":.lua<CR>")
 keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
 keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+
+keymap.set("n", "<M-j>", "<cmd>cnext<CR>")
+keymap.set("n", "<M-k>", "<cmd>cprev<CR>")
+
+keymap.set("n", "<space>st", function()
+  local cmd = vim.cmd
+
+  cmd.vnew()
+  cmd.term()
+  cmd.wincmd("J")
+  vim.api.nvim_win_set_height(0, 5)
+end)
